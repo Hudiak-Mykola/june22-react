6d00af80d91e35048d5232065baa0cbf9ff7d5ca
Hudiak-Mykola <mykola.hudiak@gmail.com>
2022-09-21 13:40:31 +0300

    hw2

---
+++ src/component/Users/Users.js
@@ -0,0 +1,28 @@
+import {useEffect, useState} from "react";
+
+import {userService} from "../../services";
+import User from "../User/User";
+import UserForm from "../UserForm/UserForm";
+
+
+function Users() {
+    const [users,setUsers]=useState([])
+
+    useEffect(()=>{
+        userService.getAll().then(value => {console.log(value.data)
+            setUsers(value.data)
+        })
+
+    },[ ])
+
+    return(
+        <div className={'mainDiv'}>
+            <UserForm/>
+            <hr/>
+            {users.map((user,id)=> (<User user={user} key={id}/>))}
+        </div>
+    )
+}
+
+
+export default Users;
